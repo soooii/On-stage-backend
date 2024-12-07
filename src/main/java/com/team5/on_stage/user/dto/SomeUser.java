cec89e0a1020a0d2d821cbc974704282bc05ef98
thoon <winwin_th@naver.com>
2024-12-07 16:33:13 +0900

    feat: Projections 사용을 위한 SomeUser 구현

---
+++ src/main/java/com/team5/on_stage/user/dto/SomeUser.java
@@ -0,0 +1,33 @@
+package com.team5.on_stage.user.dto;
+
+import com.team5.on_stage.user.entity.OAuth2Domain;
+import com.team5.on_stage.user.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SomeUser {
+
+    private String username;
+
+    private String nickname;
+
+    private String description;
+
+    private String profileImage;
+
+    private Role role;
+
+    private OAuth2Domain oAuth2Domain;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime deactivatedAt;
+}
